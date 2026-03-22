4e61c9a916853506a7361d1bc0456df522f350f5
Zimeng Yang <zimengyang8@gmail.com>
2026-03-22 16:01:02 -0400

    Initial Commit

---
+++ SnacktacularUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnacktacularUI
+//
+//  Created by Zimeng Yang on 3/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Zimeng Yang on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
